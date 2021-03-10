f5cc30f56398132e89589c2c64f39f62951c2f11
ausiaam03 <77216397+ausiaam03@users.noreply.github.com>
2021-03-10 15:34:01 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,15 @@
+//False
+// false
+// 0
+// ""
+// udenfined
+// null
+// True
+// '0',' '[]
+
+let name = 12;
+if (name || name == 0) {
+    console.log('condition is true');
+} else {
+    console.log('condition is false');
+}
